989ab810b4b9ea5e943e47e78b6970c89d3678d1
Mateus Schverz <mate_fs@hotmail.com>
2023-01-09 23:18:25 -0300

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
+var cors = require('cors')
+
+
+
+server.use(middlewares);
+server.use(router);
+server.use(cors)
+server.listen(port);
